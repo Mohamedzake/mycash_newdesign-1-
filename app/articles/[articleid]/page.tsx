1cb72c881a02a94d8428648923289de5a882b79e
Mohamedzake <zakem7671@gmail.com>
2025-02-25 01:28:02 +0200

    h

--- app/articles/[articleid]/page.tsx
+++ app/articles/[articleid]/page.tsx
@@ -1,23 +1,19 @@
 // import { articles } from "../articles";
 
-// import React from "react";
+import React from "react";
 
-// export async function generateStaticParams() {
-//   const articles = [{ articleid: '1' }, { articleid: '2' }, { articleid: '3' }]; // Replace with real data source
+export async function generateStaticParams() {
+  const articles = [{ articleid: "1" }, { articleid: "2" }, { articleid: "3" }]; // Replace with real data source
 
-//   return articles;
-// }
+  return articles;
+}
 
+const ArticleDetails = ({ params }: { params: { articleid: string } }) => {
+  // const id = params.articleid;
+  // console.log(id);
+  // const unwrappedParams = React.use(params);
 
+  return <div className="p-6">Details for the post </div>;
+};
 
-// const ArticleDetails =  ({params}: {params: {articleid: string}}) => {
-// // const id = params.articleid;
-// // console.log(id);
-// const unwrappedParams = React.use(params);
-
-//   return (
-//     <div className="p-6">Details for the post </div>
-//   );
-// };
-
-// export default ArticleDetails;
+export default ArticleDetails;
