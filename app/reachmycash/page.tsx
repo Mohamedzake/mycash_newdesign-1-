da7404d6056db338d078930bd2987d9bed153a73
Mohamedzake <zakem7671@gmail.com>
2025-02-25 01:23:43 +0200

    h

--- app/reachmycash/page.tsx
+++ app/reachmycash/page.tsx
@@ -5,9 +5,9 @@ import Header from "../_components/Header";
 import WhyMyCash from "../_components/WhyMyCash";
 import LandingForm from "../_components/LandingForm";
 import WhyChooseMyCash from "../_components/WhyChooseMyCash";
-import CompanyLogos from "../_components/CompanyLogos";
-import CompanyLogos2 from "../_components/CompanyLogos2";
-import CompanyLogos3 from "../_components/CompanyLogos3";
+// import CompanyLogos from "../_components/CompanyLogos";
+// import CompanyLogos2 from "../_components/CompanyLogos2";
+// import CompanyLogos3 from "../_components/CompanyLogos3";
 import WhatsApp from "../_components/WhatsApp";
 import { InfiniteMovingCards } from "../_components/ui/infinite-moving-cards";
 
