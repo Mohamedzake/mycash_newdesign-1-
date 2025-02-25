be4b11d78993b37b3a5e2ddbee6225911c55d3a0
Mohamedzake <zakem7671@gmail.com>
2025-02-25 16:34:01 +0200

    h

--- app/_components/FreeTrialButton.tsx
+++ app/_components/FreeTrialButton.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const FreeTrialButton = () => {
   const t = useTranslations("Globals");
   return (
-    <div className="flex flex-row justify-center gap-x-2 px-1 py-1">
+    <div className="flex flex-row justify-center gap-x-2 px-1 pb-2">
       <Link
         href="https://play.google.com/store/apps/details?id=com.codeIn.myCash&hl=ar"
         className="block text-white py-3 px-4 lg:py-5 lg:px-6 text-xs xs:px-12 col-span-5 md:text-lg lg:text-2xl bg-primary-green hover:bg-white hover:text-primary-green transition rounded-full font-bold"
